6a0592a2daa4cdc2774aff4e3f2860650ec10706
Ariel Ortiz <arielortiz.dev@gmail.com>
2023-11-14 15:46:33 -0800

    Initial Commit

---
+++ RickAndMortyGraphQL/RickAndMortyGraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyGraphQLApp.swift
+//  RickAndMortyGraphQL
+//
+//  Created by Ariel Ortiz on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyGraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
